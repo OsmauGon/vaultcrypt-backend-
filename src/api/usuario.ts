aa6e2f1f916b45e96143b6db446914ae8ad65e42
OsmauGon <mauricio7892@hotmail.com>
2025-11-25 23:19:01 -0300

    Empezamos a trabajar en el GET  de usuario.ts

--- src/api/usuario.ts
+++ src/api/usuario.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 import jwt from 'jsonwebtoken'
 import { IncomingMessage, ServerResponse } from 'http';
 import { Usuario } from '../types/user';
@@ -29,7 +30,7 @@ const usuariosSimualdos :Usuario[] = [
     }
 ];
 export default async function usersHandler(req: VercelRequest, res: VercelResponse){
-    if(req.method === 'POST'){
+    if(req.method === 'POST'){//LISTO!!
         const {name,emailPrincipal, password, secretWord} = req.body;
         if(!emailPrincipal || !password || !secretWord || !name){
             res.status(400).json({message: 'Faltan campos requeridos'})
@@ -62,6 +63,7 @@ export default async function usersHandler(req: VercelRequest, res: VercelRespon
         
     }
     else if(req.method === 'GET'){
+        //esta funcion es para devolver al propio usuario mediante el token
         const authHeader = req.headers['authorization'];
         if(!authHeader || !authHeader.startsWith('Bearer ')){
             res.status(401).json({message: 'Token no proporcionado'})
@@ -69,19 +71,38 @@ export default async function usersHandler(req: VercelRequest, res: VercelRespon
         }
         const token = authHeader.split(' ')[1]
         try{
-            const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {email: string};
-            const usuario = usuariosSimualdos.find(u => u.emailPrincipal === decoded.email)
+            const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {emailPrincipal: string};
+            /* const usuario = usuariosSimualdos.find(u => u.emailPrincipal === decoded.email)
             //aqui va la logica de base de datos
             if(!usuario){
+                res.status(404).json({message: 'Usuario no encontrado'})
+                return;
+            } */
+            const user = await prisma.usuario.findUnique({ where: { emailPrincipal: decoded.emailPrincipal } })
+            if(!user){
                 res.status(404).json({message: 'Usuario no encontrado'})
                 return;
             }
             res.status(200).json({
                 message: 'Usuario autenticado',
-                email: usuario.emailPrincipal
+                datosDEusurario: {
+                    id: user.id,
+                    name: user.name,
+                    emailList: user.emailList,
+                    secretWord: crypto.createHash("sha256").update(user.secretWord).digest("hex"),
+                    role: user.role,
+
+                }
             })
-        } catch (error){
-            res.status(401).json({message: 'Token invalido o expirado'})
+        } catch (error :any){
+            if (error.name === "TokenExpiredError") {
+            return res.status(401).json({ error: "Token expirado" });
+            }
+            if (error.name === "JsonWebTokenError") {
+            return res.status(401).json({ error: "Token inválido" });
+            }
+            return res.status(500).json({ message: "Error: ",error });
+
         }
     }
     else if(req.method === 'PUT'){
